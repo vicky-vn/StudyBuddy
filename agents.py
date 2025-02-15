24f4e38323638d43c8173f9910d00c47f3eb5893
Rustem Izmailov <rkizmailov@gmail.com>
2025-02-15 18:25:07 -0500

    pdf read implemented

--- agents.py
+++ agents.py
@@ -1,45 +1,51 @@
 import os
+import fitz
+from dotenv import load_dotenv
 from openai import OpenAI
 from autogen import ConversableAgent, UserProxyAgent, AssistantAgent, runtime_logging, Agent
 
+
+def read_pdf(file_path):
+    text = ""
+    with fitz.open(file_path) as doc:
+        for page in doc:
+            text += page.get_text("text") + "\n"
+    return text
+
+
 difficulty_level = 'Intermediate'
-with open("info.txt", "r", encoding="utf-8") as file:
-    text = file.read()
+
+# with open("info.txt", "r", encoding="utf-8") as file:
+#     text = file.read()
+
+
+text = read_pdf('text.pdf')
 
 language = 'English'
 
 config_list = [
     {
         "model": "gpt-4o-mini",
-        "api_key": "My_key"
+        "api_key": os.environ.get("OPENAI_API_KEY")
     }
 ]
-
-with open("summary.txt", "r", encoding="utf-8") as file:
-        summary_message= file.read()
-
-with open("summary.txt", "r", encoding="utf-8") as file:
-        reviewer_message= file.read()
 
 # Summary Agent
 summary_agent = AssistantAgent(
     name="SummaryAgent",
-    system_message= f"""You are an AI that summarizes text at different difficulty levels. 
-                     You must adjust the complexity based on the user's request: 
-                    'Beginner' (very simple), 'Intermediate' (easy to read), 'Professional' (formal & detailed).
-                    User level is {difficulty_level}, User language is {language} 
-                    You will also communicate with ReviseAgent who will prompt you with improvments to make in the summary, to which you must improve the summary taking all information into account""",
+    system_message= f"""You are an AI that summarizes text at different difficulty levels. There are three levels: 'Beginner' (very simple), 'Intermediate' (easy to read), 'Professional' (formal & detailed).
+You must make summary in {language} and adjust the complexity based on the user's request: {difficulty_level}
+You will also communicate with ReviseAgent who will prompt you with improvments to make in the summary, to which you must improve the summary taking all information into account""".strip(),
     llm_config={"config_list": config_list},
 )
 
 
 revise_agent = AssistantAgent(
     name="ReviseAgent",
-    system_message=f"""You are an AI that revises the summarized text at different difficulty levels and communicates with SummaryAgent to improve its summary. "
-                    You must better adjust the complexity based on the user's request: 
-                    You must determine if the summary can be improved given the original message and chat history
-                    'Beginner' (very simple), 'Intermediate' (easy to read), 'Professional' (formal & detailed).
-                    You must prompt/provide suggestion in bulletpoint format to SummaryAgent who will take your input and improve the summary""",
+    system_message=f"""You are an AI that revises the summarized text at {difficulty_level} difficulty levels and communicates with SummaryAgent to improve its summary. "
+You must better adjust the complexity based on the user's requested difficulty level: {difficulty_level}.
+You must determine if the summary can be improved given the original message and chat history
+You must prompt/provide suggestion in bulletpoint format to SummaryAgent who will take your input and improve the summary""",
     llm_config={"config_list": config_list},
     
 )
@@ -52,5 +58,5 @@ response = revise_agent.initiate_chat(
     max_turns=2,
 )
 
-print(response)
+print(response.chat_history[-1].get('content'))
 
