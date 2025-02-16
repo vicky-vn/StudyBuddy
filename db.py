791cc571b3eb92947856f5eea5eb743a6096a50b
Vignesh Natarajan <vikky33.nv@gmail.com>
2025-02-15 21:01:20 -0500

    Integrated agents to deal with summarization as well multifile input enabled

--- db.py
+++ db.py
@@ -4,10 +4,6 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client['studybuddy']
 
 user_input_collection = db['user_input']
-summary_collection = db['summary']
 
 def get_user_input_collection():
     return user_input_collection
-
-def get_summary_collection():
-    return summary_collection
